e491291f6ad2f7420043268e8c5b82a1737886ea
Marishel Lochan <marishel.lochan@my.uwi.edu>
2025-11-16 15:16:05 -0400

    Game class

---
+++ src/main/java/com/group13/State/GameState.java
@@ -0,0 +1,7 @@
+package com.group13.State;
+
+public abstract class GameState {
+    
+    abstract void startgame();
+    abstract void endgame();
+}
--- src/main/java/com/group13/State/LoadingState.java
+++ src/main/java/com/group13/State/LoadingState.java
@@ -2,5 +2,15 @@ package com.group13.State;
 
 public abstract class LoadingState extends GameState {
     // Common functionality for loading states can be added here
-    
+    @Override
+    void startgame() {
+
+        // Implementation for starting the game from loading state
+    }
+
+    @Override
+
+    void endgame() {
+        // Implementation for ending the game from loading state
+    }
 }
