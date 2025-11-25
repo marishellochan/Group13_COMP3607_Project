adf95a880f1c63ab181ee4851af63b114d629959
Aaron <aaronsookoo101@gmail.com>
2025-11-25 15:36:20 -0400

    Fix caseId parameters in GameController logging methods for proper process mining format

--- src/main/java/com/group13/Singelton/GameController.java
+++ src/main/java/com/group13/Singelton/GameController.java
@@ -35,7 +35,7 @@ public class GameController  { // this is what the UI screens will communicate w
         game.end();
     }
 
-    public void initializePlayers(ArrayList<String> playerNames) {
+    public void initializePlayers(String caseId, ArrayList<String> playerNames) {
         ArrayList<Player> players = new ArrayList<>();
         
         for (String name : playerNames) {
@@ -43,7 +43,7 @@ public class GameController  { // this is what the UI screens will communicate w
             players.add(player);
             
             // Create log entry
-            LogEntry entry = LogEntry.createPlayerJoinedEvent(
+            LogEntry entry = LogEntry.createPlayerJoinedEvent(caseId, 
                 String.valueOf(player.getPlayerId()), 
                 player.getPlayerName());
             game.notifyEventLogger(entry);
@@ -84,9 +84,10 @@ public class GameController  { // this is what the UI screens will communicate w
         return false;
     }
 
-    public void selectCategory(String category) {
+    public void selectCategory(String caseId, String category) {
         Player currentPlayer = game.getTurnManager().getCurrentPlayer();
         LogEntry entry = LogEntry.createSelectCategoryEvent(
+            caseId, 
             String.valueOf(currentPlayer.getPlayerId()), 
             category
         );
@@ -103,12 +104,13 @@ public class GameController  { // this is what the UI screens will communicate w
         return q != null && !q.isAnswered();
     }
 
-    public Question selectQuestion(String category, int value) {
+    public Question selectQuestion(String caseId, String category, int value) {
         Player currentPlayer = game.getTurnManager().getCurrentPlayer();
         Question question = game.getGameData().getQuestionByCategoryAndValue(category, value);
         
         if (question != null) {
             LogEntry entry = LogEntry.createSelectQuestionEvent(
+                caseId, 
                 String.valueOf(currentPlayer.getPlayerId()),
                 category, 
                 value
@@ -131,9 +133,13 @@ public class GameController  { // this is what the UI screens will communicate w
         }
         
         question.setAnswered();
+
+        //use new caseId from turn manager
+        String caseId = game.getTurnManager().getCurrentCaseId();
         
         // Logging
         LogEntry entry = LogEntry.createAnswerQuestionEvent(
+            caseId,
             String.valueOf(currentPlayer.getPlayerId()),
             question.getCategory(),
             question.getValue(),
@@ -144,7 +150,7 @@ public class GameController  { // this is what the UI screens will communicate w
         game.notifyEventLogger(entry);
         
         if (correct) {
-            LogEntry scoreEntry = LogEntry.scoreUpdatedEvent(
+            LogEntry scoreEntry = LogEntry.scoreUpdatedEvent(caseId, 
                 String.valueOf(currentPlayer.getPlayerId()), 
                 currentPlayer.getScore()
             );
